187ea219ab5098ae7ae47c2be51f79c07fded601
Rizzle Ann Mae V. Santos <rizzleannmaes@gmail.com>
2024-03-11 09:06:49 +0800

    register js

---
+++ register.js
@@ -0,0 +1,15 @@
+let btnRegister = document.querySelector('#btnRegister');
+let txtUsernsmer = document.querySelector("#txtUsername");
+let txtPassword = document.querySelector("#txtPassword");
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
+let txtAge = document.querySelector("#txtAge");
+
+btnRegister.onclick = function() {
+register(txtUsername.value, txtPassword.value, txtConfirmPassword.value, txtAge.value);
+}
+
+function register (username, password, confirmPassword, age) {
+    console.log(username, password, confirmPassword, age);  
+    //alert("Register button has been clicked.")
+
+}
